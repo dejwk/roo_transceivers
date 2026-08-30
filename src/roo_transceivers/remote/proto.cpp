e6f84f200570e3003d538c6cb04a48d3ec9b2fb0
Dawid Kurzyniec <dawid.kurzyniec@gmail.com>
2026-08-30 02:34:11 +0200

    Fix zero-initilization.

--- src/roo_transceivers/remote/proto.cpp
+++ src/roo_transceivers/remote/proto.cpp
@@ -6,13 +6,13 @@ namespace roo_transceivers {
 namespace proto {
 
 roo_transceivers_ServerMessage SrvInit() {
-  roo_transceivers_ServerMessage msg = roo_transceivers_ServerMessage_init_zero;
+  roo_transceivers_ServerMessage msg = {};
   msg.which_contents = roo_transceivers_ServerMessage_init_tag;
   return msg;
 }
 
 roo_transceivers_ServerMessage SrvFullUpdateBegin() {
-  roo_transceivers_ServerMessage msg = roo_transceivers_ServerMessage_init_zero;
+  roo_transceivers_ServerMessage msg = {};
   msg.which_contents =
       roo_transceivers_ServerMessage_transceiver_update_begin_tag;
   msg.contents.transceiver_update_begin.delta = false;
@@ -20,7 +20,7 @@ roo_transceivers_ServerMessage SrvFullUpdateBegin() {
 }
 
 roo_transceivers_ServerMessage SrvDeltaUpdateBegin() {
-  roo_transceivers_ServerMessage msg = roo_transceivers_ServerMessage_init_zero;
+  roo_transceivers_ServerMessage msg = {};
   msg.which_contents =
       roo_transceivers_ServerMessage_transceiver_update_begin_tag;
   msg.contents.transceiver_update_begin.delta = true;
@@ -28,7 +28,7 @@ roo_transceivers_ServerMessage SrvDeltaUpdateBegin() {
 }
 
 roo_transceivers_ServerMessage SrvUpdateEnd() {
-  roo_transceivers_ServerMessage msg = roo_transceivers_ServerMessage_init_zero;
+  roo_transceivers_ServerMessage msg = {};
   msg.which_contents =
       roo_transceivers_ServerMessage_transceiver_update_end_tag;
   return msg;
@@ -36,7 +36,7 @@ roo_transceivers_ServerMessage SrvUpdateEnd() {
 
 roo_transceivers_ServerMessage SrvDescriptorAdded(
     int key, const roo_transceivers_Descriptor& descriptor) {
-  roo_transceivers_ServerMessage msg = roo_transceivers_ServerMessage_init_zero;
+  roo_transceivers_ServerMessage msg = {};
   msg.which_contents = roo_transceivers_ServerMessage_descriptor_added_tag;
   msg.contents.descriptor_added.key = key;
   msg.contents.descriptor_added.has_descriptor = true;
@@ -45,7 +45,7 @@ roo_transceivers_ServerMessage SrvDescriptorAdded(
 }
 
 roo_transceivers_ServerMessage SrvDescriptorRemoved(int key) {
-  roo_transceivers_ServerMessage msg = roo_transceivers_ServerMessage_init_zero;
+  roo_transceivers_ServerMessage msg = {};
   msg.which_contents = roo_transceivers_ServerMessage_descriptor_removed_tag;
   msg.contents.descriptor_removed.key = key;
   return msg;
@@ -53,7 +53,7 @@ roo_transceivers_ServerMessage SrvDescriptorRemoved(int key) {
 
 roo_transceivers_ServerMessage SrvDeviceAdded(const DeviceLocator& locator,
                                               int descriptor_key) {
-  roo_transceivers_ServerMessage msg = roo_transceivers_ServerMessage_init_zero;
+  roo_transceivers_ServerMessage msg = {};
   msg.which_contents = roo_transceivers_ServerMessage_device_added_tag;
   auto& payload = msg.contents.device_added;
   strncpy(payload.locator_schema, locator.schema().c_str(),
@@ -65,7 +65,7 @@ roo_transceivers_ServerMessage SrvDeviceAdded(const DeviceLocator& locator,
 
 roo_transceivers_ServerMessage SrvDevicesPreserved(int first_preserved_ordinal,
                                                    size_t count) {
-  roo_transceivers_ServerMessage msg = roo_transceivers_ServerMessage_init_zero;
+  roo_transceivers_ServerMessage msg = {};
   msg.which_contents = roo_transceivers_ServerMessage_device_preserved_tag;
   auto& payload = msg.contents.device_preserved;
   payload.prev_index = first_preserved_ordinal;
@@ -78,7 +78,7 @@ roo_transceivers_ServerMessage SrvDevicesPreserved(int first_preserved_ordinal,
 
 roo_transceivers_ServerMessage SrvDevicesModified(int prev_ordinal,
                                                   int descriptor_key) {
-  roo_transceivers_ServerMessage msg = roo_transceivers_ServerMessage_init_zero;
+  roo_transceivers_ServerMessage msg = {};
   msg.which_contents = roo_transceivers_ServerMessage_device_modified_tag;
   auto& payload = msg.contents.device_modified;
   payload.prev_index = prev_ordinal;
@@ -87,7 +87,7 @@ roo_transceivers_ServerMessage SrvDevicesModified(int prev_ordinal,
 }
 
 roo_transceivers_ServerMessage SrvDeviceRemoved(int prev_ordinal) {
-  roo_transceivers_ServerMessage msg = roo_transceivers_ServerMessage_init_zero;
+  roo_transceivers_ServerMessage msg = {};
   msg.which_contents = roo_transceivers_ServerMessage_device_removed_tag;
   auto& payload = msg.contents.device_removed;
   payload.prev_index = prev_ordinal;
@@ -95,19 +95,19 @@ roo_transceivers_ServerMessage SrvDeviceRemoved(int prev_ordinal) {
 }
 
 roo_transceivers_ServerMessage SrvReadingsBegin() {
-  roo_transceivers_ServerMessage msg = roo_transceivers_ServerMessage_init_zero;
+  roo_transceivers_ServerMessage msg = {};
   msg.which_contents = roo_transceivers_ServerMessage_readings_begin_tag;
   return msg;
 }
 
 roo_transceivers_ServerMessage SrvReadingsEnd() {
-  roo_transceivers_ServerMessage msg = roo_transceivers_ServerMessage_init_zero;
+  roo_transceivers_ServerMessage msg = {};
   msg.which_contents = roo_transceivers_ServerMessage_readings_end_tag;
   return msg;
 }
 
 roo_transceivers_ServerMessage SrvReading(const DeviceLocator& device) {
-  roo_transceivers_ServerMessage msg = roo_transceivers_ServerMessage_init_zero;
+  roo_transceivers_ServerMessage msg = {};
   msg.which_contents = roo_transceivers_ServerMessage_reading_tag;
   auto& payload = msg.contents.reading;
   strncpy(payload.device_locator_schema, device.schema().c_str(),
@@ -131,20 +131,20 @@ void AddReading(roo_transceivers_ServerMessage& reading,
 }
 
 roo_transceivers_ClientMessage ClientRequestUpdate() {
-  roo_transceivers_ClientMessage msg = roo_transceivers_ClientMessage_init_zero;
+  roo_transceivers_ClientMessage msg = {};
   msg.which_contents = roo_transceivers_ClientMessage_request_update_tag;
   return msg;
 }
 
 roo_transceivers_ClientMessage ClientRequestState() {
-  roo_transceivers_ClientMessage msg = roo_transceivers_ClientMessage_init_zero;
+  roo_transceivers_ClientMessage msg = {};
   msg.which_contents = roo_transceivers_ClientMessage_request_state_tag;
   return msg;
 }
 
 roo_transceivers_ClientMessage ClientWrite(const ActuatorLocator& actuator,
                                            float value) {
-  roo_transceivers_ClientMessage msg = roo_transceivers_ClientMessage_init_zero;
+  roo_transceivers_ClientMessage msg = {};
   msg.which_contents = roo_transceivers_ClientMessage_write_tag;
   auto& payload = msg.contents.write;
   strncpy(payload.device_locator_schema, actuator.schema().c_str(),
